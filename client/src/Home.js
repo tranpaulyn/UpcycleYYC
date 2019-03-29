c07c58f30e948c2725fb32298514cb7886f397f2
arslanah99 <Arah09@hotmail.com>
2019-03-29 21:00:55 +0000

    template for signup

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,27 +1,16 @@
 import React from 'react'
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Link } from "react-router-dom";
+
 
 class Home extends React.Component {
 
     render(){
         return(
             <div>
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-          <h1>This is something cool?</h1>
-          <p>Paulyn was here</p>
+          <h1>Main Login Page</h1>
+          <li><Link to={'/signup'}>Login</Link></li>
+          <li><Link to={'/signup'}>SignUp</Link></li>
           </div>
         )
     }
